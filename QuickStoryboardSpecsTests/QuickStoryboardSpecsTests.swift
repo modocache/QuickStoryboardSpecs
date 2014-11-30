1673946dd8abd085563aaeae4002b6d403bc608c
pawurb <p.urbanek89@gmail.com>
2014-11-30 23:54:35 +0200

    Initial Commit

---
+++ QuickStoryboardSpecsTests/QuickStoryboardSpecsTests.swift
@@ -0,0 +1,36 @@
+//
+//  QuickStoryboardSpecsTests.swift
+//  QuickStoryboardSpecsTests
+//
+//  Created by Pablo on 30/11/14.
+//  Copyright (c) 2014 Pablo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class QuickStoryboardSpecsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
